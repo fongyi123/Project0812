84d80c521ae4b7b501bdfa1cfa4ecdca4b35b968
fong-yi <you@example.com>
2020-08-14 18:01:56 +0800

    0812筆記

--- Demo/Demo_SameOriginPolicy/getTime.php
+++ Demo/Demo_SameOriginPolicy/getTime.php
@@ -1,6 +1,6 @@
 <?php
 // header("Access-Control-Allow-Origin: *");
-
+//綠燈憑證
 $curDate = date('Y-m-d H:i:s');
 echo '{"time": "' . $curDate . '"}';
 ?>
--- Lab_Integrated/app.js
+++ Lab_Integrated/app.js
@@ -34,8 +34,9 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host : '127.0.0.1',
 	user : 'root',
-	password : '',
-	database : 'labdb'
+	password : 'root',
+	port:8889,
+	database : 'labDB'
 });
 
 connection.connect(function(err) {
